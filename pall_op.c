468fe3687d64045faca50efbc77ae9c3f3bcf745
Boakye2 <nanaboakyefred2@gmail.com>
2022-06-16 22:10:59 +0200

    update

---
+++ pall_op.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * pall - Prints all the values on the stack
+ * @stack: The head of the stack
+ * @line_number: The line on which the error occurred
+ *
+ * Return: None
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *cur = NULL;
+	(void) line_number;
+
+	if (*stack)
+	{
+		cur = *stack;
+		while (cur != NULL)
+		{
+			printf("%d\n", cur->n);
+			cur = cur->next;
+		}
+	}
+}
+
